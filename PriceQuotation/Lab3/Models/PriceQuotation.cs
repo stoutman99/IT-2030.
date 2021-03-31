167dfd81be4f81fa93afdd279d863ef10f8e65a5
stoutman99 <45634563+stoutman99@users.noreply.github.com>
2021-03-31 14:13:48 -0400

    Add files via upload

---
+++ PriceQuotation/Lab3/Models/PriceQuotation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.Models
+{
+    public class PriceQuotation
+    {
+        public int Subtotal { get; set; }
+
+        public int DiscountPercent { get; set; }
+    }
+}
