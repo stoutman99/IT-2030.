633ef16fc2b9c7c91bb34ca3c7f68d8149b056f6
stoutman99 <45634563+stoutman99@users.noreply.github.com>
2021-05-04 11:54:57 -0400

    Add files via upload

---
+++ Ch16Ex1Bookstore/Bookstore/Migrations/20210504155023_AddIdentityTables.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ch16Ex1Bookstore.Migrations
+{
+    public partial class AddIdentityTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
